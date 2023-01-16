0d1e00b8bfea40f2c941a1b0f2f9e6e5959e13d7
Junior Gomes <69778545+juniorgomes21@users.noreply.github.com>
2023-01-16 00:51:30 -0300

    fix header burguer

--- src/components/VerticalLayout/Header.jsx
+++ src/components/VerticalLayout/Header.jsx
@@ -97,17 +97,6 @@ const Header = props => {
               </Link>
             </div>
 
-            <button
-              type="button"
-              onClick={() => {
-                tToggle();
-              }}
-              className="btn btn-sm px-3 font-size-16 header-item "
-              id="vertical-menu-btn"
-            >
-              <i className="fa fa-fw fa-bars" />
-            </button>
-
           </div>
           <div className="d-flex">
 
